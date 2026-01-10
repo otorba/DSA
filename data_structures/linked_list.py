309207759e7c04ac970efd979ed8b2a006dafbb2
Oleksandr Torba <o.torba@plarium.com>
2026-01-10 19:21:19 +0100

    refactor: standardize LinkedList internals by renaming private attributes and tightening encapsulation

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -4,7 +4,7 @@ T = TypeVar("T")
 
 
 class LinkedList(Generic[T]):
-    class _Node:
+    class __Node:
         def __init__(self, value: T):
             self._value = value
             self._next = None
@@ -17,42 +17,42 @@ class LinkedList(Generic[T]):
         def next(self) -> Self | None:
             return self._next
 
-    _head: _Node | None
-    _tail: _Node | None
-    _items_count: int
+    __head: __Node | None
+    __tail: __Node | None
+    __items_count: int
 
     def __init__(self):
-        self._head = self._tail = None
-        self._items_count = 0
+        self.__head = self.__tail = None
+        self.__items_count = 0
 
     def __len__(self) -> int:
-        return self._items_count
+        return self.__items_count
 
     def __iter__(self) -> Iterator[T]:
-        current = self._head
+        current = self.__head
         while current is not None:
             yield current.value
             current = current.next
 
     def append(self, value: T) -> None:
-        new_node = self._Node(value)
-        if self._head is None:
-            self._head = self._tail = new_node
+        new_node = self.__Node(value)
+        if self.__head is None:
+            self.__head = self.__tail = new_node
         else:
-            self._tail._next = new_node
-            self._tail = new_node
-        self._items_count += 1
+            self.__tail._next = new_node
+            self.__tail = new_node
+        self.__items_count += 1
 
     def prepend(self, value: T) -> None:
-        new_node = self._Node(value)
-        if self._items_count == 0:
-            self._head = self._tail = new_node
+        new_node = self.__Node(value)
+        if self.__items_count == 0:
+            self.__head = self.__tail = new_node
         else:
-            current_value = self._head
+            current_value = self.__head
             new_node._next = current_value
-            self._head = new_node
+            self.__head = new_node
 
-        self._items_count += 1
+        self.__items_count += 1
 
     def extend(self, values: Iterator[T]) -> None:
         for i in values:
@@ -61,51 +61,51 @@ class LinkedList(Generic[T]):
     def insert(self, index: int, value: T) -> None:
         if (
                 index < 0
-                or index > self._items_count
-                or (self._items_count == 0 and index > 0)
+                or index > self.__items_count
+                or (self.__items_count == 0 and index > 0)
         ):
             raise IndexError("Index out of bounds")
 
-        new_node = self._Node(value)
-        if self._head is None:
-            self._head = self._tail = new_node
-            self._items_count += 1
+        new_node = self.__Node(value)
+        if self.__head is None:
+            self.__head = self.__tail = new_node
+            self.__items_count += 1
             return
 
         if index == 0:
-            new_node._next = self._head
-            self._head = new_node
-            self._items_count += 1
+            new_node._next = self.__head
+            self.__head = new_node
+            self.__items_count += 1
             return
 
-        prev_node = self._head
+        prev_node = self.__head
         for _ in range(index - 1):
             prev_node = prev_node.next
 
         new_node._next = prev_node.next
         prev_node._next = new_node
         if new_node.next is None:
-            self._tail = new_node
+            self.__tail = new_node
 
-        self._items_count += 1
+        self.__items_count += 1
 
     def remove(self, value: T) -> bool:
-        if self._items_count == 0 or (
-                self._head is None and self._tail is None
+        if self.__items_count == 0 or (
+                self.__head is None and self.__tail is None
         ):
             return False
 
         prev_node = None
-        current = self._head
+        current = self.__head
         while current is not None:
             if current.value == value:
                 if prev_node is not None:
                     prev_node._next = current.next  # remove a node from a list
                 else:
-                    self._head = current.next
-                if current == self._tail:
-                    self._tail = prev_node
-                self._items_count -= 1
+                    self.__head = current.next
+                if current == self.__tail:
+                    self.__tail = prev_node
+                self.__items_count -= 1
                 return True
 
             prev_node = current
@@ -114,39 +114,39 @@ class LinkedList(Generic[T]):
         return False
 
     def pop_front(self) -> T:
-        if self._items_count == 0:
+        if self.__items_count == 0:
             raise IndexError("Pop from empty list")
 
-        if self._head == self._tail:
-            value_to_return = self._head.value
-            self._head = self._tail = None
-            self._items_count = 0
+        if self.__head == self.__tail:
+            value_to_return = self.__head.value
+            self.__head = self.__tail = None
+            self.__items_count = 0
             return value_to_return
 
-        node_to_remove = self._head
-        self._head = node_to_remove.next
-        self._items_count -= 1
+        node_to_remove = self.__head
+        self.__head = node_to_remove.next
+        self.__items_count -= 1
         return node_to_remove.value
 
     def pop_back(self) -> T:
-        if self._items_count == 0:
+        if self.__items_count == 0:
             raise IndexError("Pop from empty list")
 
-        if self._head == self._tail:
-            value_to_return = self._head.value
-            self._head = self._tail = None
-            self._items_count = 0
+        if self.__head == self.__tail:
+            value_to_return = self.__head.value
+            self.__head = self.__tail = None
+            self.__items_count = 0
             return value_to_return
 
         # we don't have a reference to the previous node, so we need to iterate
-        prev_node = self._head
-        while prev_node.next is not self._tail:
+        prev_node = self.__head
+        while prev_node.next is not self.__tail:
             prev_node = prev_node.next
 
-        value_to_return = self._tail.value
+        value_to_return = self.__tail.value
         prev_node._next = None
-        self._tail = prev_node
-        self._items_count -= 1
+        self.__tail = prev_node
+        self.__items_count -= 1
         return value_to_return
 
     def index_of(self, value: T) -> int | None:
