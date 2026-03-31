c6205ecd8ddae8212f5af146b0fc9f06c750f0fd
Oleksandr Torba <o.torba@plarium.com>
2026-03-31 22:01:55 +0200

    feat: implement Queue data structure with enqueue, dequeue, and peek methods, and add iterator support

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -1,2 +1,29 @@
-class Queue:
-    pass
+from typing import Generic, Iterator, TypeVar
+
+from data_structures.double_linked_list import DoubleLinkedList
+
+T = TypeVar("T")
+
+
+class Queue(Generic[T]):
+    def __init__(self):
+        self.dll = DoubleLinkedList()
+
+    def __len__(self) -> int:
+        return len(self.dll)
+
+    def enqueue(self, value: T) -> None:
+        self.dll.append(value)
+
+    def dequeue(self) -> T:
+        if not self.dll:
+            raise IndexError("Queue is empty")
+        return self.dll.pop_front()
+
+    def peek(self) -> T:
+        if not self.dll:
+            raise IndexError("Queue is empty")
+        return self.dll.get(0)
+
+    def __iter__(self) -> Iterator[T]:
+        return iter(self.dll)
